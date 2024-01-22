e560e2ed927f9cf82748a920706feda9112dc0ce
cyanazuki <67096746+cyanazuki@users.noreply.github.com>
2024-01-22 15:29:35 +0800

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='CryoParticleSeg',
+    version='0.0.1',
+    description="A particle picking pipeline for cryo-EM micrographs using semantic segmentation trained by projection of reconstructed 3D volume.",
+    author="Po-Cheng Chou",
+    author_email="cyanazuki@gmail.com",
+    url="https://github.com/cyanazuki/CryoParticleSegment",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
